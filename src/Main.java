969aa0c245fa4965c9c9b6339babab2c239204cf
Khaled ElHadary <khaledelhadary@outlook.com>
2019-12-10 22:49:49 +0200

    added func

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 
 public class Main {
-<<<<<<< HEAD
+
 	//Khaled
 	//Ehab
 
 	public static void main(String[] args) {
 		System.out.println("Khaled");
-=======
 	}
+	public void ehab() {
+		for ( int i = 0; i < 10; ++i ) {
+			System.out.println( i );
+		}
+	}
+
 }
