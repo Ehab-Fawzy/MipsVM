769618f0cd91d6ec49a6e6782c7d912eed0068ee
Khaled ElHadary <khaledelhadary@outlook.com>
2019-12-11 21:42:02 +0200

    Update MipsMemory.java

--- src/MipsMemory.java
+++ src/MipsMemory.java
@@ -13,24 +13,31 @@ public class MipsMemory {
 	public MipsMemory(int size, String base) {
 		memory = new int[size];
 		this.base = new String(base);
+		for ( int idx = 0; idx < size; ++idx ) {
+			memory[idx] = 0;
+		}
 	}
+	
 	public void write (String index, int value) {
 		int offset = getOffset(index);
 		memory[offset] = value;
 	}
+	
 	public int[] getMemory() {
 		return memory;
 	}
+	
 	public int getOffset(String index) {
 		String indexNumber = index ,baseNumber = this.base;
 		baseNumber = this.base.substring(2);
-		if (index.contains("0x")) {
-		indexNumber = index.substring(2);
-		}
+		if (index.contains("0x")) 
+			indexNumber = index.substring(2);
 		int start = Integer.parseInt(baseNumber,16);
 		int decimal = Integer.parseInt(indexNumber,16);
+		
 		return (decimal-start)/4;
 	}
+	
 	public int getValue (String hex) {
 		int offset = getOffset(hex);
 		return memory[offset];
