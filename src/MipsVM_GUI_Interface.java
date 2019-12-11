8947c95cfb88f38d4af4aa992742964c6b1861ea
Ehab-Fawzy <32040371+Ehab-Fawzy@users.noreply.github.com>
2019-12-11 18:40:10 +0200

    labels v1

--- src/MipsVM_GUI_Interface.java
+++ src/MipsVM_GUI_Interface.java
@@ -1,18 +1,30 @@
+import java.util.HashMap;
 
 public class MipsVM_GUI_Interface {
 	public static Register REG;
 	public static MipsMemory MEM;
 	
+	public static Integer pc;
+	public static HashMap<String, Integer> labelToIdx;
+	
 	public MipsVM_GUI_Interface() {
 		REG = new Register();
 		MEM = new MipsMemory(1024, "0x00001000");
+		
+		labelToIdx = new HashMap<String, Integer>();
+		collectLables(); pc = 0; 
 	}
 	
 	public void updateRegisterFile() {
-		
+
 	}
 	
 	public void updateMemory() {
+		
+	}
+	
+	
+	public void collectLables() {
 		
 	}
 	
